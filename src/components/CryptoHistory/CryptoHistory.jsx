1769b4f5f881cf85aafb2cdf11fbbf8765ea372c
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-21 17:14:15 +0300

    Task4

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,44 @@
+import PropTypes from 'prop-types';
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
+import { getFormatDate } from 'helpers/dateToNow';
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+export const CryptoHistory = ({ transactions }) => {
+  return (
+    <div>
+      <BaseTable>
+        <THead>
+          <tr>
+            <Th>№</Th>
+            <Th>PRICE</Th>
+            <Th>AMOUNT</Th>
+            <Th>DATE</Th>
+          </tr>
+        </THead>
+
+        <tbody>
+          {transactions.map(({ id, price, amount, date }, index) => {
+            return (
+              <Tr key={id}>
+                <Td>{index + 1}</Td>
+                <Td>{price}</Td>
+                <Td>{amount}</Td>
+                <Td>{getFormatDate(date)}</Td>
+              </Tr>
+            );
+          })}
+        </tbody>
+      </BaseTable>
+    </div>
+  );
+};
+
+CryptoHistory.propTypes = {
+  transactions: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      price: PropTypes.number.isRequired,
+      amount: PropTypes.number.isRequired,
+      date: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
 };
