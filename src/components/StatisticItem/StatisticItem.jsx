b12a5674138f90da943cf52619391ac998d699f6
Igor <105238566+IgorZhyvotkov@users.noreply.github.com>
2023-05-21 16:05:11 +0300

    New task

--- src/components/StatisticItem/StatisticItem.jsx
+++ src/components/StatisticItem/StatisticItem.jsx
@@ -3,7 +3,26 @@ import {
   StatisticText,
   StatisticCounter,
 } from './StatisticItem.styled';
+import PropTypes from 'prop-types';
+import { IconContext } from "react-icons";
 
-export const StatisticItem = () => {
-  return <div>StatisticBox</div>;
+export const StatisticItem = ({total, title, icon}) => {
+  return (
+    <div>
+      <StatisticBox >
+      <IconContext.Provider value={{ size: '1.5em' }}>
+  <div>
+   {icon}
+  </div>
+</IconContext.Provider>
+        <StatisticCounter>{total}</StatisticCounter>
+        <StatisticText>{title}</StatisticText>
+      </StatisticBox>
+    </div>
+  );
+};
+
+StatisticItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  total: PropTypes.number.isRequired,
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,11 +3,44 @@ import { StatisticsList, StatisticTitle } from './Statistics.styled';
 import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
+import PropTypes from 'prop-types';
 
-export const Statistics = () => {
+
+const icons = [
+  <FaRegThumbsUp />,
+  <MdPeople />,
+  <MdOutlineProductionQuantityLimits />,
+  <GiTreeDoor />,
+];
+
+export const Statistics = ({ title, stats }) => {
   return (
     <>
-      <div>StatisticsList</div>
+      <div>
+        <StatisticTitle>{title}</StatisticTitle>
+
+        <StatisticsList>
+          {stats.map(({ id, title, total }, i) => (
+            <StatisticItem
+              key={id}
+              title={title}
+              total={total}
+              icon={icons[i]}
+            />
+          ))}
+        </StatisticsList>
+      </div>
     </>
   );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      total: PropTypes.number.isRequired,
+    }).isRequired,
+  ).isRequired,
 };
