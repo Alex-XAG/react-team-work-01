e14ad1cf64292e341af7048dc3aee05c7f8a4174
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-07-20 20:18:06 +0300

    Summary

--- src/helpers/dateToNow.js
+++
@@ -1,7 +0,0 @@
-import { formatDistanceToNow, format } from 'date-fns';
-
-export const getDistanceToNow = date =>
-  formatDistanceToNow(new Date(date), { addSuffix: true });
-
-export const getFormatDate = date =>
-  format(new Date(date), 'Pp', { addSuffix: true });
